4af5e9ad5c388b1a7e136932e08f5d56b32844c3
thewarsawpakt <plexxious@outlook.com>
2022-06-27 15:14:33 -0500

    migrated to python

---
+++ app/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_socketio import SocketIO
+from flask_bcrypt import Bcrypt
+
+app = Flask(__name__)
+bcrypt = Bcrypt(app)
+db = SQLAlchemy(app)
+ws = SocketIO(app)
