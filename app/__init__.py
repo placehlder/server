93b7625283ba467e1784cfe7ab64637ca8fa7ae4
thewarsawpakt <plexxious@outlook.com>
2022-06-29 22:14:28 -0500

    set up cors so that development is easier

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_socketio import SocketIO
 from flask_bcrypt import Bcrypt
+from flask_cors import CORS
 
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
 db = SQLAlchemy(app)
 ws = SocketIO(app)
-
+cors = CORS(app, resources={r"*": {"origins": "*"}})
